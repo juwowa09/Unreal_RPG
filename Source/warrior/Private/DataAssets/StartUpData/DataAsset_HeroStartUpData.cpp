46d9da7f742af7b7a5b5941a5f3e24a2115343b9
juwowa09 <juwowa09@gmail.com>
2025-10-14 18:41:52 +0900

    Hero 데이터에셋(어빌리티) 어빌리티 시스템에 등록

--- Source/warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,30 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 
+#include "WarriorDebugHelper.h"
+
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilitytoGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive,
+	int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FWarriorHeroAbilitySet& Abilityset : HeroStartUpAbilitySets)
+	{
+		if (!Abilityset.IsValid()) continue;
+
+		FGameplayAbilitySpec AbilitySpec(Abilityset.AbilitytoGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.GetDynamicSpecSourceTags().AddTag(Abilityset.InputTag);
+		
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
