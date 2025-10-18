9848f975529fe71790c3e0942eb6ba1fd8ab7626
juwowa09 <juwowa09@gmail.com>
2025-10-18 15:24:24 +0900

    전역 헬퍼함수 블루프린트 라이브러리 추가

---
+++ Source/warrior/Private/WarriorFunctionLibrary.cpp
@@ -0,0 +1,59 @@
+// WonUngJu All Rights Reserved
+
+
+#include "WarriorFunctionLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+UWarriorAbilitySystemComponent* UWarriorFunctionLibrary::NativeGetWarriorASCFromActor(AActor* InActor)
+{
+	check(InActor);
+	// 액터가 IAbilitySystemInterface 상속했으면 그 액터가 가지는 ASC 리턴, 아니면 nullptr 리턴
+	return CastChecked<UWarriorAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+	/* CastChecked 는 실패시 Crash 발생
+	 * Check 와 섞어놓음.
+	 * 실제로 빌드 해버리면 Cast와 동일한 역할 Crash 발생 X */ 
+}
+
+
+void UWarriorFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+	
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		// 동일한 태그 여러번 추가 가능 내부적으로 Count +1
+		// 그러나 지금 HasMatching 사용중이므로 단 하나만 됨
+		ASC->AddLooseGameplayTag(TagToAdd);
+		/* GiveAbility() 를 통해 어빌리티를 부여하는 순간,
+		그 어빌리티에 정의된 “GameplayTags” 정보들이 ASC로 전달되어 저장(등록). */
+	}
+}
+
+// Actor의 ASC 에서 태그 제거
+void UWarriorFunctionLibrary::RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+	
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		// 동일한 태그 여러번 제거 가능 내부적으로 Count -1
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+// C++ 버전으로 블루프린트에서 사용할 수 있도록 함수 구현
+bool UWarriorFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UWarriorAbilitySystemComponent* ASC = NativeGetWarriorASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+// 결과를 블루프린트에서 out 할 수 있게 사용
+void UWarriorFunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck,
+	EWarriorConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor,TagToCheck) ? EWarriorConfirmType::Yes : EWarriorConfirmType::No;
+}
+
---
+++ Source/warrior/Public/WarriorFunctionLibrary.h
@@ -0,0 +1,40 @@
+// WonUngJu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WarriorFunctionLibrary.generated.h"
+
+struct FGameplayTag;
+class UWarriorAbilitySystemComponent;
+
+UENUM()
+enum class EWarriorConfirmType : uint8
+{
+	Yes,
+	No
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Native 키워드 == C++ 레벨에서 정의된 함 / UFUNCTION 내부에서 호출용으로 씀
+	static UWarriorAbilitySystemComponent* NativeGetWarriorASCFromActor(AActor* InActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|FunctionLibrary")
+	static void RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove);
+
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck);
+
+	UFUNCTION(BlueprintCallable, Category="Warrior|FunctionLibrary", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EWarriorConfirmType& OutConfirmType);
+};
