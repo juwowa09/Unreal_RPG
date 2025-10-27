dc054876af1697c33312fe0a09e8a4812f64ac89
juwowa09 <juwowa09@gmail.com>
2025-10-27 21:45:17 +0900

    적에게 데미지 주는 연산 진짜 추가

--- Source/warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -3,17 +3,22 @@
 
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
 #include "AbilitySystem/WarriorAttributeSet.h"
+#include "WarriorGamePlayTags.h"
+
+#include "WarriorDebugHelper.h"
 
 struct FWarriorDamageCapture
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
 	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DamageTaken)
 
 	FWarriorDamageCapture()
 	{
-		// UWarriorAttributeSet 내부의 AttackPower, DefensePower 값을 캡쳐
+		// UWarriorAttributeSet 내부의 AttackPower, DefensePower 값을 캡쳐 source, target은 attempt 할때 필요함
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false);
-		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DamageTaken, Target, false);
 	}
 };
 
@@ -38,6 +43,92 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 	//
 	// RelevantAttributesToCapture.Add(AttackPowerCaptureDefinition);
 
+	// Effect가 Ability 에 의해 생성됐을 당시의 값을 캡쳐
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DamageTakenDef);
+}
+
+void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	// ExecutionParam = 모든 정보를 다 담고있음 == 공격자, 피격자, EffectSpec, 캡처된 Attribute 등
+	const FGameplayEffectSpec& EffectSpec = ExecutionParams.GetOwningSpec();
+
+	// EffectSpec.GetContext().GetSourceObject();
+	// EffectSpec.GetContext().GetAbility();
+	// EffectSpec.GetContext().GetInstigator();
+	// EffectSpec.GetContext().GetEffectCauser();
+	
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParameters.TargetTags = EffectSpec.CapturedTargetTags.GetAggregatedTags();
+	
+	float SourceAttackPower = 0.f;
+	// 실제로 캡쳐된 공격력(이 시점에서 완전 snap)과 태그들을 바탕으로 수식을 SourceAttackPower에 계산해서 저장하는 함수
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluateParameters, SourceAttackPower);
+	Debug::Print(TEXT("Source Attack Power"), SourceAttackPower);
+	
+	float BaseDamage = 0.f;
+	int32 UsedLightAttackComboCount = 0;
+	int32 UsedHeavyAttackComboCount = 0;
+
+	// SetByCaller 에 저장해둔 값을 모두 가져와서 적용하는 부분
+	for (const TPair<FGameplayTag,float>& TagMagnitude : EffectSpec.SetByCallerTagMagnitudes)
+	{
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGamePlayTags::Shared_SetByCaller_BaseDamage))
+		{
+			BaseDamage = TagMagnitude.Value;
+			Debug::Print(TEXT("BaseDamage"), BaseDamage);
+		}
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGamePlayTags::Player_SetByCaller_AttackType_Light))
+		{
+			UsedLightAttackComboCount = TagMagnitude.Value;
+			Debug::Print(TEXT("UsedLightAttackComboCount"), UsedLightAttackComboCount);
+		}
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGamePlayTags::Player_SetByCaller_AttackType_Heavy))
+		{
+			UsedHeavyAttackComboCount = TagMagnitude.Value;
+			Debug::Print(TEXT("UsedHeavyAttackComboCount"), UsedHeavyAttackComboCount);
+		}
+	}
+	
+	float TargetDefensePower = 0.f;
+	// 실제로 캡쳐된 방어(이 시점에서 완전 snap)과 태그들을 바탕으로 수식을 TargetDefensePower에 계산해서 저장하는 함수
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef,EvaluateParameters,TargetDefensePower);
+	Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
+
+	// 직접 데미지를 커스텀할 수 있음
+	if (UsedLightAttackComboCount != 0)
+	{
+		// 콤보 횟수가 늘어날 수록 더욱 강해짐
+		const float DamageIncreasePercentLight = (UsedLightAttackComboCount - 1) * 0.05 + 1.f;
+
+		BaseDamage *= DamageIncreasePercentLight;
+		Debug::Print(TEXT("Scaled Base Damage Light"), BaseDamage);
+	}
+	if (UsedHeavyAttackComboCount != 0)
+	{
+		const float DamageIncreasePercentHeavy = UsedHeavyAttackComboCount * 0.15f + 1.f;
+
+		BaseDamage *= DamageIncreasePercentHeavy;
+		Debug::Print(TEXT("Scaled Base Damage Heavy"), BaseDamage);
+	}
+
+	const float FinalDamageDone = BaseDamage * SourceAttackPower / TargetDefensePower;
+	Debug::Print(TEXT("Final Damge Done"), FinalDamageDone);
+
+	if (FinalDamageDone > 0.f)
+	{
+		OutExecutionOutput.AddOutputModifier(
+			FGameplayModifierEvaluatedData(
+				GetWarriorDamageCapture().DamageTakenProperty,
+				EGameplayModOp::Override,
+				FinalDamageDone
+			)
+		);
+	}
+
 }
--- Source/warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
+++ Source/warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
@@ -16,6 +16,7 @@ class WARRIOR_API UGEExecCalc_DamageTaken : public UGameplayEffectExecutionCalcu
 
 public:
 	UGEExecCalc_DamageTaken();
-	
-	
+
+	// C++ 구현부 Native 버전 함수. 이걸 오버라이드 하면 알아서 execute에 적용이 됨
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
