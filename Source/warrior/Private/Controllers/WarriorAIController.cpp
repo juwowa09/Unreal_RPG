d045e8aa63026e2d2ebe36442cf053f2bd724125
juwowa09 <juwowa09@gmail.com>
2025-11-05 15:59:38 +0900

    적 AI Controller 추가, 시야 감지, 시야를 관리하는 perception component 추가

--- Source/warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/warrior/Private/Controllers/WarriorAIController.cpp
@@ -2,4 +2,44 @@
 
 
 #include "Controllers/WarriorAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
+#include "WarriorDebugHelper.h"
+#include "Perception/AIPerceptionComponent.h"
+
+// 기본 PathFollowingComponent 를 군중 회피(Navigation Crowd) 로 변경
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	// 
+	if (UCrowdFollowingComponent* CrowdFollowingComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingComponent is Valid"), FColor::Green);
+	}
+
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+
+	// 적만 감지, 아군이나 중립대상은 무시
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	// 시야 반경, 각도 설정
+	// LoseSightRadius가 0이면  시야를 잃는 순간 바로 감지 해제
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	// 실제로 시야, 청각 등을 관리하는 PerceptionComponent
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+
+	// 시야 감지를 등록, Dominant(지배적 감각)
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	// 새로 감지하거나 감지를 잃을 때 여기 등록한 델리게이트를 실행
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+}
--- Source/warrior/Public/Controllers/WarriorAIController.h
+++ Source/warrior/Public/Controllers/WarriorAIController.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AISenseConfig_Sight.h"
 #include "WarriorAIController.generated.h"
 
+class UAIPerceptionComponent;
+class UAISense_Sight;
 /**
  * 
  */
@@ -13,5 +16,19 @@ UCLASS()
 class WARRIOR_API AWarriorAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	// 시야, 청각 등을 관리하는 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+
+	// 시야 감지
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
