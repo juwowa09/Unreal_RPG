248d6fe5f403ecc4393985b0452e89b346fb8c4f
juwowa09 <juwowa09@gmail.com>
2025-11-11 00:24:19 +0900

    Enemy Attack Overlap 처리, Block 변수 설정

--- Source/warrior/Private/Component/Combat/EnemyCombatComponent.cpp
+++ Source/warrior/Private/Component/Combat/EnemyCombatComponent.cpp
@@ -2,13 +2,46 @@
 
 
 #include "Component/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGamePlayTags.h"
 
 #include "WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	// 하나만 적용하기 위해 (중복 적용 X)
+	if (OverlappedActors.Contains(HitActor)) return;
+	
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO:: Implement block check
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		//TODO::check if the block is valid
+		// 블록 판정
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	if (bIsValidBlock)
+	{
+		//TODO::Handle successful block
+		// 블록 한 경우 (막은 경우)
+	}
+	else
+	{
+		// Player 가 막지 못한 경우 (애너미에 Event 송신)
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGamePlayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
