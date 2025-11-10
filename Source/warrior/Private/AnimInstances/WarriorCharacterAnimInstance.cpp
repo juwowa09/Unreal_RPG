189c0f9a9b87902546863cdcaae6b3afa131bb30
juwowa09 <juwowa09@gmail.com>
2025-11-10 13:02:19 +0900

    Blend space 사용해서 strafing Animation 적용

--- Source/warrior/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
+++ Source/warrior/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "Characters/WarriorBaseCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "KismetAnimationLibrary.h"
 
 void UWarriorCharacterAnimInstance::NativeInitializeAnimation()
 {
@@ -23,4 +24,9 @@ void UWarriorCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaS
 	}
 	GroundSpeed = OwningCharacter->GetVelocity().Size2D();
 	bHasAcceleration = OwningMovementComponent->GetCurrentAcceleration().SizeSquared2D() > 0.f;
+
+	// 이동방향과 바라보는 방향의 각도차이 계산
+	LocomotionDirection = UKismetAnimationLibrary::CalculateDirection(
+		OwningCharacter->GetVelocity(),
+		OwningCharacter->GetActorRotation());
 }
--- Source/warrior/Public/AnimInstances/WarriorBaseAnimInstance.h
+++ Source/warrior/Public/AnimInstances/WarriorBaseAnimInstance.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
+
 #include "WarriorBaseAnimInstance.generated.h"
 
 /**
@@ -13,5 +15,8 @@ UCLASS()
 class WARRIOR_API UWarriorBaseAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+	bool DoesOwnerHaveTag(FGameplayTag TagToCheck) const;
 };
