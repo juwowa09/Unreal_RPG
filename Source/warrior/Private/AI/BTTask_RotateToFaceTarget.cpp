72a75789cc70a7d31d20f466193e31994cce1082
juwowa09 <juwowa09@gmail.com>
2025-11-14 19:04:28 +0900

    Enemy Rotation Task Node 추가

--- Source/warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
+++ Source/warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
@@ -3,3 +3,49 @@
 
 #include "AI/BTTask_RotateToFaceTarget.h"
 
+#include "BehaviorTree/BlackboardData.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = TEXT("Native Rotate to Face Target Actor");
+
+	AnglePrecision = 10.f;
+	RotationInterSpeed = 5.f;
+
+	// 프레임마다 Tick 함수 호출
+	bNotifyTick = true;
+	// Task 종료요청 콜백함수허용 (데이터 정리)
+	bNotifyTaskFinished = true;
+	// AI마다 독립된 인스턴스 노드를 생성할지 ( Task 내부 멤버 변수 사용시 True 해야 멤버 변수를 AI 가 공유하지 않음 )
+	bCreateNodeInstance = false;
+
+	// Task Node 초기화
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	// Blackboard Key Selector에 ‘Actor 타입만 선택하도록’ 필터
+	InTargetToFaceKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass,InTargetToFaceKey),AActor::StaticClass());
+}
+
+// 블랙보드 에셋을 통해 Key 들을 받아올 수 있도록
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	// 블랙보드에서 키 받아와서 드롭다운 할 수 있게 함
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetToFaceKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+
+	return	FString::Printf(TEXT("Smoothly rotates to face %s Key until the angle precision %s is reached"), *KeyDescription, *FString::SanitizeFloat(AnglePrecision));
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetTaskMemory);
+}
--- Source/warrior/Public/AI/BTTask_RotateToFaceTarget.h
+++ Source/warrior/Public/AI/BTTask_RotateToFaceTarget.h
@@ -6,6 +6,23 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "BTTask_RotateToFaceTarget.generated.h"
 
+// Task Node 전용 NodeMemory == AI 마다 따로 존재
+struct FRotateToFaceTargetTaskMemory
+{
+	TWeakObjectPtr<APawn> OwningPawn;
+	TWeakObjectPtr<AActor> TargetActor;
+
+	bool isValid() const
+	{
+		return OwningPawn.IsValid() && TargetActor.IsValid();
+	}
+	void Reset()
+	{
+		OwningPawn.Reset();
+		TargetActor.Reset();
+	}
+};
+
 /**
  * 
  */
@@ -13,5 +30,25 @@ UCLASS()
 class WARRIOR_API UBTTask_RotateToFaceTarget : public UBTTaskNode
 {
 	GENERATED_BODY()
+
+	UBTTask_RotateToFaceTarget();
+
+	//~Begin UBTNode Interface
+	// Black Board 에셋을 통해 노드 초기화
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual uint16 GetInstanceMemorySize() const override;
+	virtual FString GetStaticDescription() const override;
+	//~End UBTNode Interface
+
+	// 오차 허용 각도 (멈출 각도)
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float AnglePrecision;
+
+	// 회전 속도
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float RotationInterSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	FBlackboardKeySelector InTargetToFaceKey;
 	
 };
