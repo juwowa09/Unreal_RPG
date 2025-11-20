dee86570f3e094ae46b1ce4f7a712223f63f1189
juwowa09 <juwowa09@gmail.com>
2025-11-20 16:55:19 +0900

    Box Trace 를 통해 Lock 가능한 Actor 찾는 함수, 변수 추가

--- Source/warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -3,13 +3,17 @@
 
 #include "AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h"
 
+#include "WarriorDebugHelper.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Characters/WarriorHeroCharacter.h"
+
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
 {
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
+	TryLockOnTarget();
 	
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
 void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle,
@@ -17,4 +21,45 @@ void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandl
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	// 결과로 받아올 배열
+	TArray<FHitResult> BoxTraceHits;
+
+	// 현재 캐릭터 위치 + 앞방향으로 탐색, 박스를 start -> end 까지 sweep 하면서
+	// BoxTrace Channel 안에 있는것들을 충돌했는지 검사
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(), // 시작위치, 끝 위치
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + GetHeroCharacterFromActorInfo()->GetActorForwardVector() * BoxTraceDistancce,
+		TraceBoxSize / 2.f,		// 박스의 크기 설정
+		GetHeroCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(), // 박스의 앞면 설정
+		BoxTraceChannel,	// 감지할 타입 설정
+		false,
+		TArray<AActor*>(),		// 무시할 타입 설정 (세부 타입)
+		bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,	// 디버깅 설정
+		BoxTraceHits,	 // 결과 
+		true		// 자신 무시
+	);
+
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		// 액터일 경우
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetHeroCharacterFromActorInfo())
+			{
+				AvailableActorsToLock.AddUnique(HitActor);
+
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }
--- Source/warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -19,4 +19,30 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	//~ End UGameplayAbility Interface
+
+private:
+	// Lock 실행하는 함수
+	void TryLockOnTarget();
+
+	// 박스 밀면서 Lock 가능한 Actor 찾는 함수
+	void GetAvailableActorsToLock();
+
+	// 박스 거리 어디까지 탐색
+	UPROPERTY(EditDefaultsOnly,Category="Target Lock")
+	float BoxTraceDistancce = 5000.f;
+
+	// 박스 크기 설정
+	UPROPERTY(EditDefaultsOnly,Category="Target Lock")
+	FVector TraceBoxSize = FVector(5000.f,5000.f,300.f);
+
+	// 충돌 감지할 타입
+	UPROPERTY(EditDefaultsOnly,Category="Target Lock")
+	TArray<TEnumAsByte<EObjectTypeQuery> > BoxTraceChannel;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Target Lock")
+	bool bShowPersistentDebugShape;
+
+	// Lock 가능한 액터들 저장
+	UPROPERTY()
+	TArray<AActor*> AvailableActorsToLock;
 };
