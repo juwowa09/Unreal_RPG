3870ee7b3ee2b9a628378245176d067bd44ab005
juwowa09 <juwowa09@gmail.com>
2025-11-30 02:31:55 +0900

    Boss 쫄 소환 Ability Task 세팅

--- Source/warrior/Private/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.cpp
+++ Source/warrior/Private/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.cpp
@@ -3,3 +3,18 @@
 
 #include "AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h"
 
+UAbilityTask_WaitSpawnEnemies* UAbilityTask_WaitSpawnEnemies::WaitSpawnEnemies(UGameplayAbility* OwningAbility,
+	FGameplayTag EventTag, TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn, int32 NumToSpawn,
+	const FVector& SpawnOrigin, float RandomSpawnRadius, const FRotator& SpawnRotation)
+{
+	// Task 가 어떤 Ability 의 Task 인지 초기화
+	UAbilityTask_WaitSpawnEnemies* Node = NewAbilityTask<UAbilityTask_WaitSpawnEnemies>(OwningAbility);
+	Node->CachedEventTag = EventTag;
+	Node->CachedSoftEnemyClassToSpawn = SoftEnemyClassToSpawn;
+	Node->CachedSpawnOrigin = SpawnOrigin;
+	Node->CachedRandomSpawnRadius = RandomSpawnRadius;
+	Node->CachedSpawnRotation = SpawnRotation;
+	Node->CachedNumToSpawn = NumToSpawn;
+
+	return Node;
+}
--- Source/warrior/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
+++ Source/warrior/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
@@ -6,6 +6,9 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "AbilityTask_WaitSpawnEnemies.generated.h"
 
+class AWarriorEnemyCharacter;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitSpawnEnemiesDelegate, const TArray<AWarriorEnemyCharacter*>&, SpawnEnemies);
 /**
  * 
  */
@@ -13,5 +16,30 @@ UCLASS()
 class WARRIOR_API UAbilityTask_WaitSpawnEnemies : public UAbilityTask
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Warrior|AbilityTasks", meta=(DisplayName = "Wait Gameplay Event And Spawn Enemies", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true", NumToSpawn = "1", RandomSpawnRadius = "200"))
+	static UAbilityTask_WaitSpawnEnemies* WaitSpawnEnemies(
+		UGameplayAbility* OwningAbility,
+		FGameplayTag EventTag,
+		TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn,
+		int32 NumToSpawn,
+		const FVector& SpawnOrigin,
+		float RandomSpawnRadius,
+		const FRotator& SpawnRotation
+		);
+
+	UPROPERTY(BlueprintAssignable)
+	FWaitSpawnEnemiesDelegate OnSpawnFinished;
 	
+	UPROPERTY(BlueprintAssignable)
+	FWaitSpawnEnemiesDelegate DidNotSpawn;
+
+private:
+	FGameplayTag CachedEventTag;
+	TSoftClassPtr<AWarriorEnemyCharacter> CachedSoftEnemyClassToSpawn;
+	int32 CachedNumToSpawn;
+	FVector CachedSpawnOrigin;
+	float CachedRandomSpawnRadius;
+	FRotator CachedSpawnRotation;
 };
