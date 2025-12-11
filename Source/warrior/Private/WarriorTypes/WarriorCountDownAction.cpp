2f93ffbe0479ec470f3756efccea49583ac19901
juwowa09 <juwowa09@gmail.com>
2025-12-11 23:34:25 +0900

    LatentAction Update 함수 구현

--- Source/warrior/Private/WarriorTypes/WarriorCountDownAction.cpp
+++ Source/warrior/Private/WarriorTypes/WarriorCountDownAction.cpp
@@ -3,6 +3,63 @@
 
 #include "WarriorTypes/WarriorCountDownAction.h"
 
+#include "WarriorDebugHelper.h"
+
+void FWarriorCountDownAction::UpdateOperation(FLatentResponse& Response)
+{
+	// Cancel 해야하는지 check
+	if (bNeedToCancel)
+	{
+		// Output 에 Cancel 됐음을 알림
+		CountDownOutput = EWarriorCountDownActionOutput::Cancelled;
+
+		/* true = Latent Action 종
+		 * Callback Target 에서 ExecutionFunction 실행,
+		 * OutputLink 번호 연결
+		 */
+		Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
+
+		return;
+	}
+
+	// 실행시간이 넘어갔을 경우 Complete
+	if (ElapsedTimeSinceStart >= TotalCountDownTime)
+	{
+		
+		// Output 에 Cancel 됐음을 알림
+		CountDownOutput = EWarriorCountDownActionOutput::Completed;
+
+		/* true = Latent Action 종
+		 * Callback Target 에서 ExecutionFunction 실행,
+		 * OutputLink 번호 연결
+		 */
+		Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
+		return;
+	}
+
+	// Interval 계속증가
+	if (ElapsedInterval < UpdateInterval)
+	{
+		ElapsedInterval += Response.ElapsedTime();
+	}
+	else
+	{
+		// Update 주기마다 실행, 경과시간 계산
+		ElapsedTimeSinceStart += UpdateInterval > 0.f ? UpdateInterval : Response.ElapsedTime();
+
+		// 남은 시간 계산
+		OutRemainingTime = TotalCountDownTime - ElapsedTimeSinceStart;
+
+		// 현재 상태를 Output 에 연결
+		CountDownOutput = EWarriorCountDownActionOutput::Updated;
+		// 다음 트리거를 호출함? Output Link를
+		Response.TriggerLink(ExecutionFunction,OutputLink,CallbackTarget);
+
+		// 주기 다시 0으로 업데이트
+		ElapsedInterval = 0;
+	}
+}
+
 void FWarriorCountDownAction::CancelAction()
 {
 	bNeedToCancel = true;
--- Source/warrior/Public/WarriorTypes/WarriorCountDownAction.h
+++ Source/warrior/Public/WarriorTypes/WarriorCountDownAction.h
@@ -10,7 +10,8 @@ class FWarriorCountDownAction : public FPendingLatentAction
 public:
 	FWarriorCountDownAction(float InTotalCountDownTime,float InUpdatedInterval,float& InOutRemainingTime,
 		EWarriorCountDownActionOutput& InCountDownOutput, const FLatentActionInfo& LatentInfo)
-			: bNeedToCancel(false),
+			:
+	bNeedToCancel(false),
 	TotalCountDownTime(InTotalCountDownTime),
 	UpdateInterval(InUpdatedInterval),
 	OutRemainingTime(InOutRemainingTime),
@@ -22,18 +23,31 @@ public:
 	ElapsedTimeSinceStart(0.f)
 	{}
 
+	// Tick 을 통해 계속 Update 하는 함수
+	virtual void UpdateOperation(FLatentResponse& Response) override;
+
 	// 현재 액션 Cancel
 	void CancelAction();
 	
 private:
+	// Latent Cancel 여부
 	bool bNeedToCancel;
+	// 총 수행시간
 	float TotalCountDownTime;
+	// 어느 주기로 Update 하는지
 	float UpdateInterval;
+	// 얼마나 남았는지 계산
 	float& OutRemainingTime;
+	// Latent Action의 결과
 	EWarriorCountDownActionOutput& CountDownOutput;
+	// CallbackTarget 에서 호출할 함수 이름
 	FName ExecutionFunction;
+	// Output Pin 의 Index
 	int32 OutputLink;
+	// Latent Action 을 호출자 
 	FWeakObjectPtr CallbackTarget;
+	// 다음 주기까지 실행 시간
 	float ElapsedInterval;
+	// 총 수행한 시간
 	float ElapsedTimeSinceStart;
 };
