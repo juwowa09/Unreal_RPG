07812fa2fd74578e371dfcaa7637e98bacca29fa
juwowa09 <juwowa09@gmail.com>
2025-12-30 18:21:41 +0900

    Loading 화면 추가

--- Source/warrior/Private/WarriorGameInstance.cpp
+++ Source/warrior/Private/WarriorGameInstance.cpp
@@ -2,6 +2,32 @@
 
 
 #include "WarriorGameInstance.h"
+#include "MoviePlayer.h"
+
+void UWarriorGameInstance::Init()
+{
+	Super::Init();
+
+	// Map Load 전 실행하는 Delegate 에 함수 바인딩
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this,&ThisClass::OnPreLoadMap);
+	// Map Load 후 실행하는 Delegate 에 함수 바인딩
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this,&ThisClass::OnPostLoadMapWithWorld);
+}
+
+void UWarriorGameInstance::OnPreLoadMap(const FString& MapName)
+{
+	FLoadingScreenAttributes LoadingScreenAttributes;
+	LoadingScreenAttributes.bAutoCompleteWhenLoadingCompletes = true;
+	LoadingScreenAttributes.MinimumLoadingScreenDisplayTime = 2.f;
+	LoadingScreenAttributes.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+
+	GetMoviePlayer()->SetupLoadingScreen(LoadingScreenAttributes);
+}
+
+void UWarriorGameInstance::OnPostLoadMapWithWorld(UWorld* LoadedWorld)
+{
+	GetMoviePlayer()->StopMovie();
+}
 
 TSoftObjectPtr<UWorld> UWarriorGameInstance::GetGameLevelByTag(FGameplayTag InTag) const
 {
--- Source/warrior/Public/WarriorGameInstance.h
+++ Source/warrior/Public/WarriorGameInstance.h
@@ -35,7 +35,17 @@ class WARRIOR_API UWarriorGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
+public:
+	// Game Instance 초기화 시 한 번 호출되는 함수
+	virtual void Init();
+	
 protected:
+	// GameInstance Init 시, Map 을 Load 하기 전 준비하는 Delegate 에 등록할 함수
+	virtual void OnPreLoadMap(const FString& MapName);
+
+	// Map 을 Load 하고 난 후 Delegate 에 등록할 함수
+	virtual void OnPostLoadMapWithWorld( UWorld* LoadedWorld );
+	
 	// 레벨과 태그를 배열로 저장
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TArray<FWarriorGameLevelSet> GameLevelSets;
